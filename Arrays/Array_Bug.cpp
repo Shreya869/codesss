851180e726723ecb31c08abf574ccefc34655fab
Shreya <62697529+Shreya869@users.noreply.github.com>
2021-01-27 19:32:53 +0530

    Create Array_Bug.cpp

---
+++ Arrays/Array_Bug.cpp
@@ -0,0 +1,37 @@
+//Question
+/*
+ARRAY_BUG
+The following code is supposed to rotate the array A by B positions.
+
+So, for example,
+A : [1 2 3 4 5 6]
+B : 1
+The output :
+[2 3 4 5 6 1]
+However, there is a small bug in the problem. Fix the bug and submit the problem.
+
+*/
+
+//Solution
+
+vector<int> Solution::rotateArray(vector<int> &A, int B) 
+{
+    vector<int> ret; 
+    int j=0;
+    B=B%(A.size());
+    
+    for (int i = 0; i < A.size(); i++) {
+        if((i+B)<A.size())
+        {
+            ret.push_back(A[i + B]);
+        }
+        else
+        {
+            ret.push_back(A[j]);
+            j++;
+        }
+        
+    }
+    return ret; 
+}
+
